42d4d061eda0bfb0b7778b4e3f10685b4790785a
Tamara Kuzhel <tomikuzh@gmail.com>
2017-02-07 13:56:43 +0200

    Lesson 1 programm

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 1;
+            int n2 = 1;
+
+            Console.WriteLine("Please specify a number:");
+
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please specify one more number:");
+
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            if ( n1 % n2 == 0)
+            {
+                Console.WriteLine("Second value is divider of first!");
+            }
+
+            else
+            {
+                Console.WriteLine("Second value is NOT divider of first!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
